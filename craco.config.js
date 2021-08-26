611b6dd53dcd6d3517a178f9c4686257ac476683
FinnCowbell <thomasfnavin@gmail.com>
2021-08-26 14:31:41 -0400

    Tailwind CSS setup

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+}
